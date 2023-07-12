614af0d75a8f4f2758d9a874b775f37901597213
황제연 <kingpele9018@gmail.com>
2023-07-12 12:11:05 +0900

    Create APIfunc.py

---
+++ code/APIfunc.py
@@ -0,0 +1,33 @@
+import requests
+import json
+import Info
+
+
+#url = 'http://apis.data.go.kr/1262000/CountryCodeService2/getCountryCodeList2?serviceKey=비공개&pageNo=1&numOfRows=300'
+#response = requests.get(url)
+#contents = response.json()
+#print(contents)
+def getApi_1(res):
+    url = 'http://apis.data.go.kr/1262000/CountryCodeService2/getCountryCodeList2?serviceKey=비공개&pageNo=1&numOfRows=300&cond[country_eng_nm::EQ]='
+    url += res
+    #print(url)
+    response = requests.get(url)
+    contents = response.json()
+
+    contents_array = contents["data"]
+    if Info.Alpha2 == 1:
+        for cot in contents_array:
+            return cot["country_iso_alp2"]
+    elif Info.Alpha3 == 1:
+        for cot in contents_array:
+            return cot["iso_alp3"]
+
+def getApi_2(res):
+    url = 'http://apis.data.go.kr/1262000/CountryCodeService2/getCountryCodeList2?serviceKey=비공개&pageNo=1&numOfRows=300&cond[country_iso_alp2::EQ]='
+    url += res
+    response = requests.get(url)
+    contents = response.json()
+
+    contents_array = contents["data"]
+    for cot in contents_array:
+        return cot["country_eng_nm"]
