79aaf00eecee07e267ccf18fb9dab6b70c5439ab
황제연 <kingpele9018@gmail.com>
2023-07-14 10:27:45 +0900

    Update func.py

--- code/func.py
+++ code/func.py
@@ -1,59 +1,94 @@
-import tkinter
-import customtkinter
-import func
+import tkinter.messagebox
+import GUI
+import Info
+import APIfunc
 
-window = customtkinter.CTk()
-window.geometry("500x500")
-window.title("MCCS")
-window.resizable(False,False)
-window.grid_columnconfigure((0,2),weight=1)
-window.grid_rowconfigure((2,20),weight=1)
+# 왼쪽 라디오 버튼 누를때 호출되는 함수
+def RBChk1(event):
+    if Info.Alpha2 == 0 and Info.Alpha3 == 0:
+        Info.Alpha2 = 1
+        GUI.radiobutton1.select()
+        GUI.radiobutton1.grid(row=0, column=0, padx=0, pady=20, sticky="e")
+    else:
+        Info.Alpha2 = 0
+        GUI.radiobutton1.deselect()
+        GUI.radiobutton1.grid(row=0, column=0, padx=0, pady=20, sticky="e")
 
-# 한 열에서 여러 행 배치하기 위해 만든 프레임
-frame1 = customtkinter.CTkFrame(window, fg_color="transparent")
+# 오른쪽 라디오 버튼 누를때 호출되는 함수 -> ver1.0 미지원
+def RBChk2(event):
+    GUI.radiobutton2.deselect()
+    tkinter.messagebox.showinfo(title="ver 1.0", message="현재 버전에서 지원하지 않습니다.")
+    if Info.Alpha3 == 0 and Info.Alpha2 ==0:
 
-# 라디오버튼 정렬을 위한 빈 라벨
-align_label1 = customtkinter.CTkLabel(window, text="", anchor="center")
+        '''Info.Alpha3 = 1
+        GUI.radiobutton2.select()
+        GUI.radiobutton2.grid(row=0, column=2, padx=0, pady=20, sticky="w")'''
+    else:
+        '''Info.Alpha3 = 0
+        GUI.radiobutton2.deselect()
+        GUI.radiobutton2.grid(row=0, column=2, padx=0, pady=20, sticky="w")'''
 
-# ISO Alpha를 체크할 수 있는 라디오 버튼, ISO Alpha2가 기본값
-radiobutton1 = customtkinter.CTkRadioButton(window, text="ISO alpha2")
-radiobutton2 = customtkinter.CTkRadioButton(window, text="ISO alpha3")
-radiobutton1.select()
+# 변환 버튼 눌렀을 때 옆 텍스트 상자로 변환해서 넣어주는 함수
+def GetText1(event):
+    # 작성한 값을 가져와서 변수에 저장
+    Info.CountryName_S = GUI.textbox1.get("1.0", "end-1c")
+    Info.CountryName_L = Info.CountryName_S.split("\n")
+    Info.CountryCode_S = ""
 
-# 텍스트 상자 제목
-text_label1 = customtkinter.CTkLabel(window, text="국가명")
-text_label2 = customtkinter.CTkLabel(window, text="국가 코드")
+    #값을 가져오기 위한 과정
+    for name in Info.CountryName_L:
+        # 먼저 메모리 저장소를 확인
+        for key in Info.MemoryRepository.keys():
+            # 값이 있으면 메모리 저장소로부터 가져옴
+            if key == name:
+                Info.CountryCode_S += Info.MemoryRepository[key]
+                Info.Key_chk = 1
+                break
+            # 값이 없으면 Get 요청으로 값을 가져옴
+        if Info.Key_chk != 1:
+            Info.MemoryRepository[name] = str(APIfunc.getApi_1(name))
+            Info.CountryCode_S += Info.MemoryRepository[name]
+        else:
+            Info.Key_chk = 0
+        Info.CountryCode_S += "\n"
+    # 옆 텍스트 상자로 값을 넣어주고 GUI 배치
+    Info.CountryCode_S = Info.CountryCode_S[:-1]
+    GUI.textbox2.delete("1.0", "end-1c")
+    GUI.textbox2.insert("0.0", Info.CountryCode_S)
+    GUI.textbox2.grid(row=2, column=2, padx=0, pady=0)
 
-# 값을 입력할 수 있는 텍스트 상자
-textbox1 = customtkinter.CTkTextbox(window, width=170, height=300,border_width=2, border_color="black")
-textbox2 = customtkinter.CTkTextbox(window, width=170, height=300,border_width=2, border_color="black")
 
-# 변환 버튼
-button1 = customtkinter.CTkButton(frame1, text="변환", width=50)
-button2 = customtkinter.CTkButton(frame1, text="변환", width=50)
+# 변환 버튼 눌렀을 때 옆 텍스트 상자로 변환해서 넣어주는 함수
+def GetText2(event):
+    # 작성한 값을 가져와서 변수에 저장
+    Info.CountryCode_S = GUI.textbox2.get("1.0", "end-1c")
+    Info.CountryCode_L = Info.CountryCode_S.split("\n")
+    Info.CountryName_S = ""
 
-# 화살표 캔버스
-canvas1 = tkinter.Canvas(frame1, width=50, height=20, bg="#ebebeb", bd=0, highlightthickness=0)
-canvas2 = tkinter.Canvas(frame1, width=50, height=20, bg="#ebebeb", bd=0, highlightthickness=0)
-canvas1.create_line(0, 10, 50, 10, arrow="last", width=2)
-canvas2.create_line(50, 10, 0, 10, arrow="last", width=2)
+    # 값을 가져오기 위한 과정
+    for name in Info.CountryCode_L:
+        #ISO Alpha3 변환을 요청할 때, 알림창 띄어주는 함수
+        if len(name) == 3:
+            tkinter.messagebox.showinfo(title="ver1.0", message="현재 버전에서 지원하지 않습니다")
+            break
+        # 먼저 메모리 저장소를 확인
+        for key in Info.MemoryRepository.keys():
+            # 값이 있으면 메모리 저장소로부터 가져옴
+            if key == name:
+                Info.CountryName_S += Info.MemoryRepository[key]
+                Info.Key_chk = 1
+                break
+            # 값이 없으면 Get 요청으로 값을 가져옴
+        if Info.Key_chk != 1:
+            Info.MemoryRepository[name] = str(APIfunc.getApi_2(name))
+            Info.CountryName_S += Info.MemoryRepository[name]
+        else:
+            Info.Key_chk = 0
+        Info.CountryName_S += "\n"
 
-# GUI 배치 코드들
-align_label1.grid(row=0,column=1,padx=0,pady=20, sticky="e")
-radiobutton1.grid(row=0,column=0,padx=0,pady=20, sticky="e")
-radiobutton2.grid(row=0,column=2,padx=0,pady=20, sticky="w")
-frame1.grid(row=2,column=1,padx=0,pady=0,sticky="ns")
-text_label1.grid(row=1,column=0,padx=0,pady=40)
-text_label2.grid(row=1,column=2,padx=0,pady=40)
-textbox1.grid(row=2, column=0, padx=0, pady=0)
-textbox2.grid(row=2, column=2, padx=0, pady=0)
-button1.grid(row=0,column=0,padx=0,pady=30)
-button2.grid(row=3,column=0,padx=0,pady=0)
-canvas1.grid(row=1, column=0,padx=0,pady=15)
-canvas2.grid(row=2, column=0,padx=0,pady=40)
+    # 옆 텍스트 상자로 값을 넣어주고 GUI 배치
+    Info.CountryName_S = Info.CountryName_S[:-1]
+    GUI.textbox1.delete("1.0", "end-1c")
+    GUI.textbox1.insert("0.0", Info.CountryName_S)
+    GUI.textbox1.grid(row=2, column=0, padx=0, pady=0)
 
-# GUI 이벤트 호출
-radiobutton1.bind("<ButtonRelease-1>", func.RBChk1)
-radiobutton2.bind("<ButtonRelease-1>", func.RBChk2)
-button1.bind("<ButtonRelease-1>", func.GetText1)
-button2.bind("<ButtonRelease-1>", func.GetText2)
