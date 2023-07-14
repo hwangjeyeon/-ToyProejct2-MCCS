f3c3a1ea97466cad7a04685db2eb6b127b58f2f2
황제연 <kingpele9018@gmail.com>
2023-07-14 10:28:36 +0900

    Update APIfunc.py

--- code/APIfunc.py
+++ code/APIfunc.py
@@ -1,42 +1,43 @@
-
 import requests
-import json
 import Info
 
 
-#url = 'http://apis.data.go.kr/1262000/CountryCodeService2/getCountryCodeList2?serviceKey=비공개&pageNo=1&numOfRows=300'
-#response = requests.get(url)
-#contents = response.json()
-#print(contents)
 def getApi_1(res):
+    # 공공데이터 포탈에서 제공하는 값이랑 우리가 평소에 생각하는 국가명이랑 달라서 자동으로 바뀌도록 설정한 코드
     if res == "United States":
         res = "United States of America"
     if res == "Hong Kong":
         res = "HongKong"
     if res == "Hong Kong SAR China":
         res = "HongKong"
-    else:
-        pass
-    url = 'http://apis.data.go.kr/1262000/CountryCodeService2/getCountryCodeList2?serviceKey=비공개&pageNo=1&numOfRows=300&cond[country_eng_nm::EQ]='
+
+    # Get 요청을 위한 코드
+    url = 'http://apis.data.go.kr/1262000/CountryCodeService2/getCountryCodeList2' \
+          '?serviceKey=비공개' \
+          '&pageNo=1&numOfRows=300&cond[country_eng_nm::EQ]='
     url += res
-    #print(url)
     response = requests.get(url)
     contents = response.json()
 
+    # 받아온 데이터를 저장 후 리턴
     contents_array = contents["data"]
     if Info.Alpha2 == 1:
-        for cot in contents_array:
-            return cot["country_iso_alp2"]
+        for content in contents_array:
+            return content["country_iso_alp2"]
     '''elif Info.Alpha3 == 1:
         for cot in contents_array:
             return cot["iso_alp3"]'''
 
 def getApi_2(res):
-    url = 'http://apis.data.go.kr/1262000/CountryCodeService2/getCountryCodeList2?serviceKey=비공개&pageNo=1&numOfRows=300&cond[country_iso_alp2::EQ]='
+    # Get 요청을 위한 코드
+    url = 'http://apis.data.go.kr/1262000/CountryCodeService2/getCountryCodeList2?' \
+          'serviceKey=비공개' \
+          '&pageNo=1&numOfRows=300&cond[country_iso_alp2::EQ]='
     url += res
     response = requests.get(url)
     contents = response.json()
 
+    # 받아온 데이터를 저장 후 리턴
     contents_array = contents["data"]
-    for cot in contents_array:
-        return cot["country_eng_nm"]
+    for content in contents_array:
+        return content["country_eng_nm"]
